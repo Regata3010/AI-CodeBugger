98600d538457c294af1ce8bd15b0307b579a8cfb
Arav Pandey <aravpandey3010@gmail.com>
2025-09-21 22:31:42 -0400

    Fixing Credentials Path

--- backend/api/routes/vscode.py
+++ backend/api/routes/vscode.py
@@ -122,7 +122,8 @@ async def vscode_chat_wrapper(request: ConversationalVSCodeChatRequest):
     """
     
     try:
-        os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/home/nuclearreactor3010/AI-CodeBugger/backend/regata-2ca53-df75398184a5.json"
+        if not os.getenv("GOOGLE_APPLICATION_CREDENTIALS"):
+            os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/app/credentials/google-credentials.json"
         # os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/AI Bugger/backend/regata-2ca53-df75398184a5.json"
         openai_api_key = os.getenv("OPENAI_API_KEY")
         if not openai_api_key:
